38a1ffa7848c6118939a3b806fbd2a55aefe1d12
TakeruUshio <ta-ushio@groovenauts.jp>
2023-05-15 20:19:53 +0900

    fix indent

--- src/foo.spec.ts
+++ src/foo.spec.ts
@@ -1,6 +1,6 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('basic test', async ({page}) => {
   await page.goto('https://playwright.dev/');
   const name = await page.innerText('.navbar__title');
   expect(name).toBe('Playwright');
